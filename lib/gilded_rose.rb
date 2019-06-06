a504a087f0a4d0366d2cd1b5351bebad2b74fdd4
Alasdair <alasdair123@hotmail.co.uk>
2019-06-06 13:29:14 +0100

    add separate method to update normal foo item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,47 +5,63 @@ class GildedRose
     @items = items
   end
 
+  def normal_item_name
+    item.name
+  end
+
+  def normal_update(item)
+    item.sell_in -= 1
+    return if item.quality == 0
+
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in <= 0
+  end
+
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-            if item.sell_in < 6
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          item.quality = item.quality + 1 if item.quality < 50
-        end
-      end
+      normal_update(item) if item.name = 'foo'
     end
   end
+
+  #     if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+  #       if item.quality > 0
+  #         if item.name != 'Sulfuras, Hand of Ragnaros'
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.sell_in < 11
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #           if item.sell_in < 6
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != 'Sulfuras, Hand of Ragnaros'
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != 'Aged Brie'
+  #         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.quality > 0
+  #             if item.name != 'Sulfuras, Hand of Ragnaros'
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         item.quality = item.quality + 1 if item.quality < 50
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 class Item
